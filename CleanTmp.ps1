a6f7ff8f86b1acafdecdec67b9f6542ef7e901f1
KHT@air <tan.k.h@juno.com>
2021-06-28 17:18:27 -0700

    CleanTmp.ps1

---
+++ CleanTmp.ps1
@@ -0,0 +1,3 @@
+Write-Host "Congratulations"
+$tempfolders = @("C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*")
+Remove-Item $tempfolders -force -recurse
