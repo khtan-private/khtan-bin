56c436bd77eb51c224a0b4ad88b327e3717178b5
Kwee Tan <kwee@eaglecapsoftware.com>
2023-04-19 18:04:28 -0700

    k-work.bat --refactored-> {k-ecco, k-emacs}.bat

---
+++ k-ecco.bat
@@ -0,0 +1,13 @@
+@echo off
+rem Spacemacs naming conventions is <spacemacs-type>-<machinename>-<os>
+if /I "%computername%"=="kwee1" (
+   echo "%computername% is supported"
+   start "C:\Program Files (x86)\ECCO\ecco32.exe" "g:\My Drive\za\eaglecap.eco"
+   start "C:\Program Files (x86)\ECCO\ecco32.exe" "g:\My Drive\za\family-usa.eco"
+) else if /I "%computername%"=="air" (
+  echo "%computername% is supported"
+  start "C:\Program Files (x86)\ECCO\ecco32.exe" "C:\Users\tankh\GDrive\tan.k.h.usa@gmail.com\za\eaglecap.eco"
+  start "C:\Program Files (x86)\ECCO\ecco32.exe" "C:\Users\tankh\GDrive\tan.k.h.usa@gmail.com\za\family-usa.eco"
+) else (
+  echo "Warning: %computername% not yet supported"
+)
--- k-emacs.bat
+++ k-emacs.bat
@@ -3,13 +3,9 @@ rem Spacemacs naming conventions is <spacemacs-type>-<machinename>-<os>
 if /I "%computername%"=="kwee1" (
    echo "%computername% is supported"
    start /B emacs --with-profile spacemacs -title spacemacs-kwee1-win
-   start "C:\Program Files (x86)\ECCO\ecco32.exe" "g:\My Drive\za\eaglecap.eco"
-   start "C:\Program Files (x86)\ECCO\ecco32.exe" "g:\My Drive\za\family-usa.eco"
 ) else if /I "%computername%"=="air" (
   echo "%computername% is supported"
   start /B emacs --with-profile spacemacs -title spacemacs-air-win
-  start "C:\Program Files (x86)\ECCO\ecco32.exe" "C:\Users\tankh\GDrive\tan.k.h.usa@gmail.com\za\eaglecap.eco"
-  start "C:\Program Files (x86)\ECCO\ecco32.exe" "C:\Users\tankh\GDrive\tan.k.h.usa@gmail.com\za\family-usa.eco"
 ) else (
   echo "Warning: %computername% not yet supported"
 )
