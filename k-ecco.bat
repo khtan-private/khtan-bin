ae2643b873cd9a97cc52e83c5bd3b56b3aa43c5e
Kwee Tan <kwee@eaglecapsoftware.com>
2023-05-08 13:06:02 -0700

    open eaglecap.eco last

--- k-ecco.bat
+++ k-ecco.bat
@@ -2,12 +2,12 @@
 rem Spacemacs naming conventions is <spacemacs-type>-<machinename>-<os>
 if /I "%computername%"=="kwee1" (
    echo "%computername% is supported"
-   start "C:\Program Files (x86)\ECCO\ecco32.exe" "g:\My Drive\za\eaglecap.eco"
    start "C:\Program Files (x86)\ECCO\ecco32.exe" "g:\My Drive\za\family-usa.eco"
+   start "C:\Program Files (x86)\ECCO\ecco32.exe" "g:\My Drive\za\eaglecap.eco"
 ) else if /I "%computername%"=="air" (
   echo "%computername% is supported"
-  start "C:\Program Files (x86)\ECCO\ecco32.exe" "C:\Users\tankh\GDrive\tan.k.h.usa@gmail.com\za\eaglecap.eco"
   start "C:\Program Files (x86)\ECCO\ecco32.exe" "C:\Users\tankh\GDrive\tan.k.h.usa@gmail.com\za\family-usa.eco"
+  start "C:\Program Files (x86)\ECCO\ecco32.exe" "C:\Users\tankh\GDrive\tan.k.h.usa@gmail.com\za\eaglecap.eco"
 ) else (
   echo "Warning: %computername% not yet supported"
 )
