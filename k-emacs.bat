4afc5885e6ea15f921ecc5960ccde73593546353
Kwee Tan <kwtan@firstam.com>
2023-07-04 16:46:18 -0700

    Added firstAm machine, changed naming convention of emacs window

--- k-emacs.bat
+++ k-emacs.bat
@@ -1,11 +1,15 @@
 @echo off
 rem Spacemacs naming conventions is <spacemacs-type>-<machinename>-<os>
+set eprofile=spacemacs
 if /I "%computername%"=="kwee1" (
    echo "%computername% is supported"
-   start /B emacs --with-profile spacemacs -title spacemacs-kwee1-win
+   start /B emacs --with-profile spacemacs -title "%computername%-win-sp"
 ) else if /I "%computername%"=="air" (
   echo "%computername% is supported"
-  start /B emacs --with-profile spacemacs -title spacemacs-air-win
+  start /B emacs --with-profile spacemacs -title "%computername%-win-sp"
+) else if /I "%computername%"=="ap-nylwne40vfl0" (
+  echo "%computername% is supported"
+  start /B emacs --with-profile %eprofile% -title "%computername%-win-%eprofile%"
 ) else (
   echo "Warning: %computername% not yet supported"
 )
